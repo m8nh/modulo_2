229a743bdac795d8662f8b7e338e9e6ce264cdc8
minh <minhnghiemquangminh75@gmail.com>
2022-11-07 10:51:59 +0700

    first commit

---
+++ week1/th/spread-operator-example.js
@@ -0,0 +1,11 @@
+
+// Example 3: We will use the spread operator with objects
+// cars1 object
+const cars1 = {
+    Brand : "BMW",
+    Color : "RED"
+}
+// copy cars1 object using spread
+// operator to create cars2 object
+const cars2 = {...cars1};
+console.log(cars2);
