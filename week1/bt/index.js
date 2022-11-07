9cb3a14f62170db0107d0a79e28381e5244f1904
minh <minhnghiemquangminh75@gmail.com>
2022-11-07 14:18:16 +0700

    .

---
+++ week1/bt/index.js
@@ -0,0 +1,71 @@
+/*
+function lastIndexOf(arr, elt, start) {
+
+  for (let i = start - 1; i >= 0; i--)
+
+
+    if (arr[i] === elt) return i
+
+
+  return -1
+
+
+}
+
+
+console.log(lastIndexOf([1, 2, 1, 2], 2))
+
+
+const inventory = [
+
+  {type:   "machine", value: 5000},
+
+
+  {type:   "machine", value:  650},
+
+
+  {type:      "duck", value:   10},
+
+
+  {type: "furniture", value: 1200},
+
+
+  {type:   "machine", value:   77}
+
+]
+
+let totalMachineValue = VIẾT_BIỂU_THỨC_TẠI_ĐÂY//
+
+console.log(totalMachineValue)
+
+
+let profile = {
+    fname: 'Code',
+    lname: 'Gym',
+    bday: new Date('1979-01-02')
+}
+
+let fname = profile.fname;
+let lname = profile.lname;
+
+console.log(fname,lname);
+
+
+function detectCollision(objects, point) {
+    for (let i = 0; i < objects.length; i++) {
+
+    let object = objects[i];
+
+    if (point.x >= point.x && object.x <= objects.x + objects.width &&
+        point.y >= point.y && object.y <= objects.y + objects.height)
+        return object
+} 
+}
+const myObjects = [
+    {x: 10, y:20, with:30, height:30},
+    {x: -40, y: 20, width: 30, height: 30},
+    {x:   0, y:  0, width: 10, height:  5}
+]
+
+console.log(detectCollision(myObjects, {x:4, y:2}))
+*/
