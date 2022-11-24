7e04b608dd004597b81d1dd405f640b07dffac88
minh <minhnghiemquangminh75@gmail.com>
2022-11-24 08:15:56 +0700

    .

---
+++ week3/bt/debug.ngoaile/b1.ts
@@ -0,0 +1,14 @@
+function calculator(number1, number2, operator) {
+    let result;
+    if (operator == '+') {
+        result = number1 + number2;
+    } else if (operator == '-') {
+        result = number1 - number2;
+    } else if (operator == '*') {
+        result = number1 * number2;
+    } else if (operator == '/') {
+        result = number1 / number2;
+    }
+    return result;
+}
+
